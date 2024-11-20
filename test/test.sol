be1c86965ba6b6ae5af04cd72eff5f5819054b2e
zjeen-max <facillliteras@gmail.com>
2024-11-20 16:44:48 +0800

    add test

--- test/test.sol
+++ test/test.sol
@@ -1,11 +1,21 @@
 //SPDX-License-Identifier: UNLICENSED
-//pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 contract HelloWord{
-    string name = "aaa11";
-    function getHello() view public returns(string memory){
-        return name;
+    struct Info {
+        string phrase;
+        uint256 id;
+        address addr;
     }
-    function changeName(string memory _name) public{
-        name = _name;
-    }
+    //Info[] infos;
+    mapping(uint256=>Info) infoMapping;
+   function sayHello(uint256 _id) public view returns(string memory){
+    return addinfo(infoMapping[_id].phrase);
+   }
+   function setHello(uint256 _id,string memory str) public {
+    Info memory info = Info(str,_id,msg.sender);
+    infoMapping[_id]=info;
+   }
+   function addinfo(string memory str) internal pure returns(string memory){
+    return string.concat(str, "from frank's contract.");
+   }
 }
